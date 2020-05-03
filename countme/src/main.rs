a44e03ff08a66f447a8512083ed98360263d95b3
Ali Soleimani <ali.soleimani@booking.com>
2020-05-03 21:04:18 +0200

    be human

--- countme/src/main.rs
+++ countme/src/main.rs
@@ -1,56 +1,34 @@
-use tokio::net::TcpStream;
-use kmp;
-use std::{net::Shutdown, str};
-use tokio::net::TcpListener;
-use tokio::prelude::*;
+use actix_web::{web, App, HttpResponse, HttpServer};
+use bytes::Bytes;
 use std::sync::atomic::{AtomicIsize, Ordering};
+use std::str;
 
 static mut COUNT: AtomicIsize = AtomicIsize::new(0);
-const OK: &[u8] = b"HTTP/1.1 200 OK\r\n\r\n";
-const CONTENT_LEN: &[u8] = b"Content-Length:";
-const NEW_LINE: &[u8] = b"\r\n";
-const HEADER_END: &[u8] = b"\r\n\r\n";
-async fn handle_client(mut stream: TcpStream) {
-    let mut header = [0 as u8; 2048];
-    
-    match stream.read(&mut header).await {
-        Ok(size) => {
-            if header[0] == ('G' as u8) {
-                unsafe {
-                    stream.write_all(OK).await;
-                    stream.write_all(format!("{}", COUNT.load(Ordering::SeqCst)).as_bytes()).await;
-                }
-            } else {
-                let cls = kmp::kmp_find(CONTENT_LEN, &header).unwrap();
-                let cln = kmp::kmp_find(NEW_LINE, &header[cls+CONTENT_LEN.len()..]).unwrap();
-                let len = size;
-                unsafe {
-                    let cl = str::from_utf8_unchecked(&header[cls+CONTENT_LEN.len()+1..cls+CONTENT_LEN.len()+cln]).parse::<usize>().unwrap();
-                    let end = kmp::kmp_find(HEADER_END, &header[cls+CONTENT_LEN.len()+cln..]).unwrap();
-                    let header_len = cls + CONTENT_LEN.len() + cln + end + HEADER_END.len();
-                    let need_more = header_len  + cl - len;
-                    if need_more > 0 {
-                        stream.read_exact(&mut header[len..len + need_more]).await;
-                    }
-                    let input =  str::from_utf8_unchecked(&header[header_len..header_len+cl]).parse::<isize>().unwrap();
-                    COUNT.fetch_add(input, Ordering::SeqCst);
-                }
-                stream.write_all(OK).await;
-            }
-        },
-        Err(_) => {}
+
+async fn add(body: Bytes) -> HttpResponse {
+    unsafe {
+        COUNT.fetch_add(str::from_utf8_unchecked(&body).parse::<isize>().unwrap(), Ordering::SeqCst);
     }
-    stream.shutdown(Shutdown::Both).unwrap();
+    HttpResponse::Ok().body("")
+
 }
 
-#[tokio::main]
-async fn main() -> io::Result<()> {
-    let mut listener = TcpListener::bind("0.0.0.0:80").await?;
-
-    loop {
-        let (socket, _) = listener.accept().await?;
-        handle_client(socket).await;
+async fn count() -> HttpResponse {
+    unsafe {
+        HttpResponse::Ok().body(format!("{}",COUNT.load(Ordering::SeqCst)))
     }
 }
 
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::post().to(add))
+            .route("/count", web::get().to(count))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
+}
 
